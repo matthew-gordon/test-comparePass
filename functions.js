4f188cae6e9e2ad4614b1864cf126d9a651c5afd
matthew-gordon <laxredemption5525@gmail.com>
2017-03-14 15:05:09 -0600

    Add assertions, function for comparing password

--- functions.js
+++ functions.js
@@ -1,1 +1,16 @@
 'use strict';
+
+const bcrypt = require('bcryptjs');
+
+function comparePass(userPassword, databasePassword) {
+  const bool = bcrypt.compareSync(userPassword, databasePassword);
+  if(!bool) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+module.exports = {
+  comparePass
+};
---
+++ test/functions.spec.js
@@ -0,0 +1,26 @@
+'use strict';
+
+
+const chai = require('chai');
+const should = chai.should();
+const bcrypt = require('bcryptjs');
+const functions = require('../functions');
+
+describe('comparePass()', () => {
+    it('should return true if the password is correct', (done) => {
+     const salt = bcrypt.genSaltSync();
+     const hash = bcrypt.hashSync('test', salt);
+     const results = functions.comparePass('test', hash);
+     should.exist(results);
+     results.should.eql(true);
+     done();
+    });
+    it('should return false if the password is correct', (done) => {
+     const salt = bcrypt.genSaltSync();
+     const hash = bcrypt.hashSync('test', salt);
+     const results = functions.comparePass('testing', hash);
+     should.exist(results);
+     results.should.eql(false);
+     done();
+    });
+  });
